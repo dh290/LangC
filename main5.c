7e8d3eae50e24ecbec901eb0e2a8857516a898c1
Fujita <d2531061@eng.kitakyu-u.ac.jp>
2023-11-24 13:04:02 +0900

    main5

--- main5.c
+++ main5.c
@@ -4,6 +4,9 @@ int main(void){
     int x=10;
     int y=2*x;
     int z=x+y;
+    int price=500;
     printf("%d,%d,%d\n",x,y,z);
+    double price0=1.1*price;
+    printf("%f\n",price0);
     return 0;
 }
